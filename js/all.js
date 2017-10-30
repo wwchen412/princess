a8a00b16e4a1823144c0d47e00e75bd9325ea7a3
wwchen <wwchen412@gmail.com>
2017-10-30 19:24:28 +0800

    改nav 跟字型

--- js/all.js
+++ js/all.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
     $('body.hidden-load').fadeIn(200).removeClass('hidden-load');
+
+    $('.navbar-nav li .dropdown').hover(function() {
+        $(this).find('.dropdown-menu').stop(true, true).fadeIn(0);
+    }, function() {
+        $(this).find('.dropdown-menu').stop(true, true).fadeOut(0);
+    });
 });
