0eda3506e1f5778af0ab90f55f85eda8f5d38cb5
wwchen <wwchen412@gmail.com>
2017-10-31 16:51:16 +0800

    追蹤碼

--- js/all.js
+++ js/all.js
@@ -7,3 +7,14 @@ $(document).ready(function() {
         $(this).find('.dropdown-menu').stop(true, true).fadeOut(0);
     });
 });
+
+function download() {
+
+    (scupioconv = window.scupioconv || []).push({ bwp: 1160, bwpid: 'prebuy' });
+    (window._bwtm = window._bwtm || []).push({
+        "mid": 249,
+        "convid": "preconv_2",
+        "carts": [{ "itemid": 1, "price": 1, "count": 1 }]
+    });
+    window.open('http://www.princesscruises.com.tw/downloads/Majestic_Princess_2018_Brochure_PrincessCruisesTaiwan_20171012.pdf');
+}
